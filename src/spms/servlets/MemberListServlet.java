ff358bb27038ce5a1dfc95dbefb199ee0cc2e0b0
chjinlook <chjinlook@gmail.com>
2019-06-10 22:46:11 +0900

    MySQL 정상접속확인

--- src/spms/servlets/MemberListServlet.java
+++ src/spms/servlets/MemberListServlet.java
@@ -1,36 +1,77 @@
 package spms.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet("/member/list")
 public class MemberListServlet extends GenericServlet{
 
 	@Override
-	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		
 		try {
 			// TODO Auto-generated method stub
 			//1, 사용할 JDBC 드라이버를 등록하라. |
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 			//2. 드라이버를 사용하여 MySQL 서버와 연결하라.
-			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/myintranet","root","interhouse");
 			//3. 커넥션 객체로부터 SQL을 던질 객체를 준비하라.
-
+			stmt = conn.createStatement();
 			//4. SQL을 던지는 객체를 사용하여 서버에 질의하라!
-
+			rs = stmt.executeQuery(
+					"SELECT MNO,MNAME,EMAIL,CRE_DATE" + 
+					" FROM MEMBERS" +
+					" ORDER BY MNO ASC");
 			//5. 서버에 가져온 데이터를 사용하여 HTML 만들어서 웹 브라우저로 출력하라.
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>회원목록</title></head>");
+			out.println("<body><h1>회원목록</h1>");			
 			
+			while(rs.next()) {
+				out.println(
+					rs.getInt("MNO") + "," +
+					rs.getString("MNAME") + "," +
+					rs.getString("EMAIL") + "," + 
+					rs.getDate("CRE_DATE") + "<br>"
+				);
+			}		
+			out.println("</body></html>");
 		} catch(SQLException e) {
-			throw new ServletException(e);
+			e.printStackTrace();
+		} finally {
+			try {if (rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+			try {if (stmt != null) stmt.close();} catch(Exception e) {e.printStackTrace();}
+			try {if (conn != null) conn.close();} catch(Exception e) {e.printStackTrace();}			
 		}
 
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
